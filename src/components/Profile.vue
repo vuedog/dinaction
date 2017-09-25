fc544e93071c788835165e872427acf6a05a6c3f
Runrioter <runrioter@qq.com>
2017-09-25 23:57:52 +0800

    add Profile

---
+++ src/components/Profile.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <img src="../assets/profile.jpeg">
+  <p>Username: {{ username }}</p>
+  <p>Gender: {{ gender }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'profile',
+  data () {
+    return {
+      username: 'Runrioter Wung',
+      gender: 'male'
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 230px;
+  height: 230px;
+}
+</style>
+
+
