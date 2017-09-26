69edde5502e45b3e43950a33d715926ada9c03e1
Runrioter <runrioter@qq.com>
2017-09-26 23:41:57 +0800

    add missing vue

---
+++ src/components/User.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>Username: {{$route.params.username}}</div>
+    <div>
+      <router-link to="/user/runrioter">Runrioter Wung</router-link>
+      <router-link to="/user/peter">Peter</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'user',
+  beforeCreate () {
+    console.log('Before create')
+  },
+  created () {
+    console.log('Created')
+  },
+  beforeMount () {
+    console.log('Before mount')
+  },
+  mounted () {
+    console.log('Mounted')
+  },
+  beforeRouteUpdate (to, from, next) {
+    console.log('from', from, 'to', to)
+    next()
+  }
+}
+</script>
+
