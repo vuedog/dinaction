9aa9c2c4d929801054f6f4e18ed592cd5727189f
Runrioter <runrioter@qq.com>
2017-09-26 23:41:35 +0800

    use router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Profile from '@/components/Profile'
+import User from '@/components/User'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/profile',
       name: 'Profile',
       component: Profile
+    },
+    {
+      path: '/user/:username',
+      name: 'User',
+      component: User
     }
   ]
 })
