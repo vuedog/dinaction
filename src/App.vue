cbfab14d8e8b83850d654838401243ce85c44dd1
Runrioter <runrioter@qq.com>
2017-09-29 23:58:39 +0800

    Add custom components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <vd-head></vd-head>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import VdHead from '@/components/VdHeader'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    VdHead
+  }
 }
 </script>
 
